ad140d74ec726cbeb48b83b4efeb1b3f57661674
Yiling Cao <yiling.cao@gmail.com>
2015-01-06 23:40:52 +0800

    make build process multithread automatically.

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 export ARCH="arm"
-export CORES="24"
+export CORES=`getconf _NPROCESSORS_ONLN`
 export CROSS_COMPILE="arm-linux-gnueabihf-"
 export PATH="/opt/gcc-linaro-arm-linux-gnueabihf-4.8-2013.06_linux/bin:/opt/gcc-linaro-arm-linux-gnueabihf-4.8-2013.06_linux/bin:$PATH"
 
-make
+set -xe
+make -j${CORES} mx6dlsabresd 
