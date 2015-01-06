e820ad63b59a149144fbab7233eb42fd9626bd50
Yiling Cao <yiling.cao@gmail.com>
2015-01-06 22:13:17 +0800

    add make board_name to build script.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 export ARCH="arm"
-export CORES="24"
+export CORES=`getconf _NPROCESSORS_ONLN`
 export CROSS_COMPILE="arm-linux-gnueabihf-"
 export PATH="/opt/gcc-linaro-arm-linux-gnueabihf-4.8-2013.06_linux/bin:/opt/gcc-linaro-arm-linux-gnueabihf-4.8-2013.06_linux/bin:$PATH"
-
-make
+make mx6qsabresd
+make -j${CORES}
